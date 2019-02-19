28be6fb0d35f9d238664569805c7b2a2064c2337
Frances Rather <francesrather@gmail.com>
2019-02-19 18:12:43 -0500

    almostcomp

--- Christmas-Advent-Calendar_blue_24-days/js/snowfall.js
+++ Christmas-Advent-Calendar_blue_24-days/js/snowfall.js
@@ -14,26 +14,26 @@
        See the License for the specific language governing permissions and
        limitations under the License.
     ====================================================================
-    
+
     1.0
     Wanted to rewrite my snow plugin to use pure JS so you werent necessarily tied to using a framework.
     Does not include a selector engine or anything, just pass elements to it using standard JS selectors.
-    
+
     Does not clear snow currently. Collection portion removed just for ease of testing will add back in next version
-    
+
     Theres a few ways to call the snow you could do it the following way by directly passing the selector,
-    
+
         snowFall.snow(document.getElementsByTagName("body"), {options});
-    
+
     or you could save the selector results to a variable, and then call it
-        
+
         var elements = document.getElementsByClassName('yourclass');
         snowFall.snow(elements, {options});
-        
+
     Options are all the same as the plugin except clear, and collection
-    
+
     values for snow options are
-    
+
     flakeCount,
     flakeColor,
     flakeIndex,
@@ -43,7 +43,7 @@
     maxSpeed,
     round,      true or false, makes the snowflakes rounded if the browser supports it.
     shadow      true or false, gives the snowflakes a shadow if the browser supports it.
-        
+
 */
 
 // requestAnimationFrame polyfill from https://github.com/darius/requestAnimationFrame
@@ -52,7 +52,7 @@ if (!Date.now)
 
 (function() {
     'use strict';
-    
+
     var vendors = ['webkit', 'moz'];
     for (var i = 0; i < vendors.length && !window.requestAnimationFrame; ++i) {
         var vp = vendors[i];
@@ -77,17 +77,17 @@ var snowFall = (function(){
     function jSnow(){
         // local methods
         var defaults = {
-            flakeCount : 100,
-            flakeColor : '#ffffff',
+            flakeCount : 10,
+            // flakeColor : '#ffffff',
             flakeIndex: 999999,
-            minSize : 1,
-            maxSize : 5,
+            minSize : 40,
+            maxSize : 60,
             minSpeed : 1,
-            maxSpeed : 5,
+            maxSpeed : 2,
             round : true,
             shadow : false,
             collection : false,
-            image : false,
+            image : true,
             collectionHeight : 40
             },
             element = {},
@@ -109,7 +109,7 @@ var snowFall = (function(){
             },
             // random between range
             random = function random(min, max){
-                return Math.round(min + Math.random()*(max-min)); 
+                return Math.round(min + Math.random()*(max-min));
             },
             // Set multiple styles at once.
             setStyle = function(element, props)
@@ -122,7 +122,7 @@ var snowFall = (function(){
             flake = function(_x, _y, _size, _speed, _id)
             {
                 // Flake properties
-                this.id = _id; 
+                this.id = _id;
                 this.x  = _x + elLeft;
                 this.y  = _y + elTop;
                 this.size = _size;
@@ -133,35 +133,35 @@ var snowFall = (function(){
                 if(defaults.collection){
                     this.target = canvasCollection[random(0,canvasCollection.length-1)];
                 }
-                
+
                 var flakeObj = null;
-                
+
                 if(defaults.image){
-                    flakeObj = new Image();
-                    flakeObj.src = defaults.image;
+                    flakeObj = new Image('/img/boob.png');
+                    flakeObj.src = 'img/boob.png';
                 }else{
                     flakeObj = document.createElement("div");
                     setStyle(flakeObj, {'background' : defaults.flakeColor});
                 }
-                
+
                 flakeObj.className = 'snowfall-flakes';
                 flakeObj.setAttribute('id','flake-' + this.id);
                 setStyle(flakeObj, {'width' : this.size, 'height' : this.size, 'position' : 'absolute', 'top' : this.y, 'left' : this.x, 'fontSize' : 0, 'zIndex' : defaults.flakeIndex});
-        
-                // This adds the style to make the snowflakes round via border radius property 
+
+                // This adds the style to make the snowflakes round via border radius property
                 if(defaults.round){
                     setStyle(flakeObj,{'-moz-border-radius' : ~~(defaults.maxSize) + 'px', '-webkit-border-radius' : ~~(defaults.maxSize) + 'px', 'borderRadius' : ~~(defaults.maxSize) + 'px'});
                 }
-            
+
                 // This adds shadows just below the snowflake so they pop a bit on lighter colored web pages
                 if(defaults.shadow){
                     setStyle(flakeObj,{'-moz-box-shadow' : '1px 1px 1px #555', '-webkit-box-shadow' : '1px 1px 1px #555', 'boxShadow' : '1px 1px 1px #555'});
                 }
-                
+
                 document.body.appendChild(flakeObj);
-                
+
                 this.element = flakeObj;
-                
+
                 // Update function, used to update the snow flakes, and checks current snowflake against bounds
                 this.update = function(){
                     this.y += this.speed;
@@ -169,18 +169,18 @@ var snowFall = (function(){
                     if(this.y > (elTop + elHeight) - (this.size  + 6)){
                         this.reset();
                     }
-                    
+
                     this.element.style.top = this.y + 'px';
                     this.element.style.left = ~~this.x + 'px';
 
                     this.step += this.stepSize;
                     this.x += Math.cos(this.step);
-                    
+
                     if(this.x + this.size > (elLeft + elWidth) - widthOffset || this.x < widthOffset){
                         this.reset();
                     }
                 }
-                
+
                 // Resets the snowflake once it reaches one of the bounds set
                 this.reset = function(){
                     this.y = elTop;
@@ -202,7 +202,7 @@ var snowFall = (function(){
         return{
             snow : function(_element, _options){
                 extend(defaults, _options);
-                
+
                 //init the element vars
                 element = _element;
                 elHeight = element.clientHeight,
@@ -211,12 +211,12 @@ var snowFall = (function(){
                 elLeft = element.offsetLeft;
 
                 element.snow = this;
-                
+
                 // if this is the body the offset is a little different
                 if(element.tagName.toLowerCase() === 'body'){
                     widthOffset = 25;
                 }
-                
+
                 // Bind the window resize event so we can get the innerHeight again
                 window.onresize = function(){
                     elHeight = element.clientHeight;
@@ -224,7 +224,7 @@ var snowFall = (function(){
                     elTop = element.offsetTop;
                     elLeft = element.offsetLeft;
                 }
-                
+
                 // initialize the flakes
                 for(i = 0; i < defaults.flakeCount; i+=1){
                     flakeId = flakes.length;
@@ -235,7 +235,7 @@ var snowFall = (function(){
             },
             clear : function(){
                 var flakeChildren = null;
-                
+
                 if(!element.getElementsByClassName){
                     flakeChildren = element.querySelectorAll('.snowfall-flakes');
                 }else{
@@ -246,7 +246,7 @@ var snowFall = (function(){
                 while(flakeChilLen--){
                     element.removeChild(flakeChildren[flakeChilLen]);
                 }
-                
+
                 flakes = [];
                 cancelAnimationFrame(snowTimeout);
             }
